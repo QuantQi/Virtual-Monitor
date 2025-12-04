8b7378c7b62bde1edf9c0e69a24144f9e1fae418
Q <soubhiks@hotmail.com>
2025-12-04 18:06:45 +0000

    Remove .build directory from tracking

--- .build/arm64-apple-macosx/release/CNIOWASI.build/CNIOWASI.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX26.1.sdk/usr/include/Darwin.modulemap \
-  /Users/soc/Projects/temp/Virtual\ Monitor/.build/checkouts/swift-nio/Sources/CNIOWASI/CNIOWASI.c
--- .build/arm64-apple-macosx/release/CNIOWindows.build/shim.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/soc/Projects/temp/Virtual\ Monitor/.build/checkouts/swift-nio/Sources/CNIOWindows/include/module.modulemap \
-  /Users/soc/Projects/temp/Virtual\ Monitor/.build/checkouts/swift-nio/Sources/CNIOWindows/shim.c
